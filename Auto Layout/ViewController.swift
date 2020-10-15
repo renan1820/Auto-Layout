7943969eef7ba37c3dfb22fa16c5906757f52655
Renan Castro <rcribeiro@brq.com>
2020-10-15 01:39:23 -0300

    instila?

--- Auto Layout/ViewController.swift
+++ Auto Layout/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let redView = UIView()
+        
+        view.addSubview(redView
+        )
+        redView.backgroundColor = .red
+        
+        //Setando nossas Constraints de forma manual
+        redView.translatesAutoresizingMaskIntoConstraints = false
+        
+        //Config contraint Top com o layout pai e habilitando nossa constraint na nossa red view
+//        redView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
+        
+        //leadingAnchor = left // trailingAnchor = right
+//        redView.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 50).isActive = true
+        
+//        redView.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -50).isActive = true
+        
+//        redView.bottomAnchor.constraint(equalTo: view.bottomAnchor,constant: -50).isActive = true
+        
+        //Constraint Altura e largura
+        
+        redView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        redView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        
+        redView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        redView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
-
-
+    
+    
 }
 
