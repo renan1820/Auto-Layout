83efbfa0482cc0a1e6e2c74495ff06a2ff91e5c9
Renan Castro <rcribeiro@brq.com>
2020-10-15 00:37:31 -0300

    Initial Commit

---
+++ Auto Layout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout
+//
+//  Created by Renan de Castro Ribeiro on 15/10/20.
+//  Copyright © 2020 Renan de Castro Ribeiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
